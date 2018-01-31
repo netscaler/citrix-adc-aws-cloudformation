f2e1adc72a488682ab02bcc4741d0c3e62a386d1
Chiradeep Vittal <chiradeep@apache.org>
2018-01-31 10:06:32 -0500

    prepare for new workflow

--- experimental/vpx_single_nic_with_lambda_init/create-stack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-# Sample invocation to create a single NIC VPX. You HAVE to customize ParameterValues to your environment
-aws cloudformation create-stack --template-body file://ns.1nic.template --capabilities CAPABILITY_IAM --stack-name vpx-1nic-test --parameters  ParameterKey=Subnet,ParameterValue=subnet-a1b2c3d4 ParameterKey=VpcID,ParameterValue=vpc-1a2b3c4d ParameterKey=ImageName,ParameterValue=VpxStandard1000 ParameterKey=KeyName,ParameterValue=mykeyname
